057204d85c698c3394d6d8e789008cb42f410580
cawaulitra <39634179+cawaulitra@users.noreply.github.com>
2020-11-09 17:23:57 +0300

    лекция 7

--- php/7/2/upload.php
+++ php/7/2/upload.php
@@ -1,31 +1,36 @@
 <?php
 
 	/*
-		Задание 1: Реализовать у себя на сервере форму загрузки файла.
-		Также в файле upload.php продемонстрировать на экран содержимое
-		массива $_FILES, а также всех промежуточных переменных, чтобы
-		увидеть, что где хранится и в каком виде.
+		Задание 2: Реализовать у себя на сервере форму загрузки множественных
+		файлов. Добавить ограничения.
 	*/
-		
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 	$uploadOk = 1;
-	$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-	// Проверяем, является ли файл изображением
-	if (isset($_POST["submit"])) {
-		@$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]); //@ убирает Notice.
-		if ($check !== false) {
-			echo "Файл является изображением - " . $check["mime"] . ".</br></br>";
-			$uploadOk = 1;
-		}
-		else {
-			echo "Файл не является изображением.</br></br>";
+	foreach ($_FILES ["pictures"]["size"] as $key => $size) { //проверка на размер
+		if ($size > 500000) {
+			echo "Файл под номером $key слишком большой.</br>";
 			$uploadOk = 0;
 		}
 	}
-	var_dump ($_FILES);							//вывод всего массива
-	echo "</br>Папка: $target_dir";				//папка
-	echo "</br>Путь до файла: $target_file";	//путь до файла
-	echo "</br>Разрешить загрузку: $uploadOk";	//индикатор загрузки?
-	echo "</br>Тип файла: $imageFileType";		//тип изображения
+	
+	foreach ($_FILES ["pictures"]["type"] as $key => $type) { //проверка на расширение
+		if ($type != "image/jpg" && $type != "image/png" && $type != "image/gif" && $type != "image/jpeg") {
+			echo "Файл под номером $key не имеет формат jpeg, jpg, gif или png.</br>";
+			$uploadOk = 0;
+		}
+	}
+	
+	if ($uploadOk == 0) { //при любом нарушении ограничения файлы не будут скачаны
+		echo "Ваш(и) файл(ы) не был(и) загружен(ы).</br>";
+	}
+	else {
+		foreach ($_FILES ["pictures"]["error"] as $key => $error) {
+			if ($error == UPLOAD_ERR_OK) {
+				$tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+				$name = $_FILES["pictures"]["name"][$key];
+				move_uploaded_file($tmp_name, "C:/xampp/htdocs/php/7/2/img/$name");
+			}
+		}
+		echo "Ваши файлы успешно загружены!</br>";
+	}
+	//var_dump ($_FILES);
 ?>
