4be439408ba47dbbeedb6148f1cb6b3ed6c9fca2
cawaulitra <39634179+cawaulitra@users.noreply.github.com>
2021-01-15 17:55:28 +0300

    php12

---
+++ php/0/a.php
@@ -0,0 +1,22 @@
+<?php
+	/*$x = 5;
+	$y = 10;
+	echo "a: $x </br>";
+	echo "b: $y </br>";
+	$x = $x + $y;
+	$y = $x - $y;
+	$x = $x - $y;
+	echo "a: $x </br>";
+	echo "b: $y </br>";	*/
+	
+	$a = 10;
+	$b = 20;
+	$c = 6;
+	$d = 4;
+	
+	$d = $c;
+	$c = $b;
+	$b = $a;
+	
+	echo "$a </br> $b </br> $c </br> $d";
+?>
